2b6c8cc21b4f8a859a0e3bf934ad809d325784bc
Alexandre Bellandi <bellandi@gmail.com>
2022-04-01 15:18:06 -0300

    Projet TDD

---
+++ bds02/src/main/java/com/devsuperior/bds02/controller/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,45 @@
+package com.devsuperior.bds02.controller.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.devsuperior.bds02.services.exceptions.DataBaseException;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandartError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request)
+	{
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandartError err = new StandartError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setErro("Resource not found");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DataBaseException.class)
+	public ResponseEntity<StandartError> entityNotFound(DataBaseException e, HttpServletRequest request)
+	{
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandartError err = new StandartError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setErro("Database Exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
+	}
+}
